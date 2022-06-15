b18b6c24db414fe8a026c104044f7a4519b8ec2f
Ton de Ron <ton.ron@shell.com>
2022-06-15 16:08:00 +0200

    Initial commit

---
+++ DemoOpenTelemetry/Controllers/WeatherForecastController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading;
+using System.Threading.Tasks;
+using DemoOpenTelemetry.Clients;
+
+namespace DemoOpenTelemetry.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly WeatherHttpClient _httpClient;
+        private readonly JsonSerializerOptions _jsonSerializerOptions;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, WeatherHttpClient httpClient)
+        {
+            _logger = logger;
+            _httpClient = httpClient;
+            _jsonSerializerOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get(CancellationToken token = default)
+        {
+            _logger.LogInformation("Get the weather");
+            using var response = await _httpClient.Client.GetAsync("/WeatherForecast", HttpCompletionOption.ResponseHeadersRead, token);
+            response.EnsureSuccessStatusCode();
+
+            var stream = await response.Content.ReadAsStreamAsync();
+            var weatherInfo = await JsonSerializer.DeserializeAsync<IEnumerable<WeatherForecast>>(stream, _jsonSerializerOptions, token);
+            return Ok(weatherInfo);
+        }
+    }
+}
---
+++ DemoOpenTelemetry/Startup.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using DemoOpenTelemetry.Clients;
+
+namespace DemoOpenTelemetry
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddHttpClient<WeatherHttpClient>();
+            services.AddSwaggerGen();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Weather API V1");
+                c.RoutePrefix = string.Empty;
+            });
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        }
+    }
+}
